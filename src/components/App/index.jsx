3cc4acfb956919b06c94202340bcbebb3a869774
Carlos Rodríguez <racm1698@gmail.com>
2023-08-09 16:51:39 -0600

    Se agrego el paquete de react-router-dom para poder crear en enrutamiento de cada una de las paginas

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes,BrowserRouter } from 'react-router-dom';
 import {Home} from '../Home'
 import {Order} from '../Order'
 import {Orders} from '../Orders'
@@ -5,15 +6,25 @@ import {SingIn} from '../SingIn'
 import {NotFound} from '../NotFound'
 import './app.css'
 
+const AppRoutes = ()=>{
+  //hook para poder crear rutas en React
+  let routes = useRoutes([
+    {path: '/', element: <Home/>},
+    {path: '/my-order', element: <Order/>},
+    {path: '/my-orders', element: <Orders/>},
+    {path: '/sing-in', element: <SingIn/>},
+    {path: '/*', element: <NotFound/>},
+  ])
+  return routes;
+}
+
 function App() {
 
   return (
     <>
-     <Home/>
-     <Order/>
-     <Orders/>
-     <SingIn/>
-     <NotFound/>
+     <BrowserRouter>
+        <AppRoutes/>
+     </BrowserRouter>
     </>
   )
 }
