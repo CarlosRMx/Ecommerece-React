9429e70de484183efd797b5cf791734bd7a5e9a1
Carlos Rodríguez <racm1698@gmail.com>
2023-08-30 10:47:29 -0600

    Se termino de estilizar el componente que muestra cada una de las ordenes realizadas

--- src/components/ItemOrders/index.jsx
+++ src/components/ItemOrders/index.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 function ItemOrders(props) {
 
   const {date,totalItems, totalPrice}= props
   
   return (
-    <div className='flex justify-between items-center w-80 p-2 mb-3 bg-black/5 border border-black rounded-xl'>
-      <p className='flex flex-col w-full'>
-        <div>
+    <div className='flex justify-between items-center gap-4 w-80 p-3 mb-3 border-current shadow-md rounded-lg'>
+      <div className='flex flex-col w-full'>
+        <p>
           <span className='font-ligth text-medium'>{date}</span>
-        </div>
-        <div className='flex justify-between items-center'>
+        </p>
+        <p className='flex justify-between items-center'>
           <span className='text-medium font-extrabold text-green-500'>{totalItems} articles</span>
           <span className='text-xl font-medium'>$ {totalPrice}</span>
-        </div>
-      </p>
+        </p>
+      </div>
+      <ChevronRightIcon className='w-6 h-6 text-black'/>
     </div>
   )
 }
